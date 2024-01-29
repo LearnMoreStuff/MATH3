860e40cddae5d71229c925c28b10719540fa86ba
CloIT <105884295+Clo612@users.noreply.github.com>
2024-01-29 15:37:42 -0600

    Create math3addition.swift

---
+++ math3addition.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+print("Hi \(name) thank you for getting MATH3 we hope you enjoy the new and improved MATH!!!")
+Thread.sleep(forTimeInterval: 2)
+print("If you get the math wrong you start over!!")
+Thread.sleep(forTimeInterval: 4)
+print("What is \(random)")
+if let math = readLine(), math == "2" {
+    print("Thats right")
+} else {
+    print("Wrong")
+    exit(1)
+}
+Thread.sleep(forTimeInterval: 2)
+print("What is 2+2")
+if let math = readLine(), math == "4" {
+    print("Thats right")
+} else {
+    print("Wrong")
+    exit(1)
+}
+print("What is 3+3")
+if let math = readLine(), math == "6" {
+    print("Thats right")
+} else {
+    print("Wrong")
+    exit(1)
+}
+print("What is 4+4")
+if let math = readLine(), math == "8" {
+    print("Thats right")
+} else {
+    print("Wrong")
+    exit(1)
+}
+print("What is 5+5")
+if let math = readLine(), math == "10" {
+    print("Thats right")
+} else {
+    print("Wrong")
+    exit(1)
+}
+print("What is 6+6")
+if let math = readLine(), math == "12" {
+    print("Thats right")
+} else {
+    print("Wrong")
+    exit(1)
+}
+print("What is 7+7")
+if let math = readLine(), math == "14" {
+    print("Thats right")
+} else {
+    print("Wrong")
+    exit(1)
+}
+print("What is 8+8")
+if let math = readLine(), math == "16" {
+    print("Thats right")
+} else {
+    print("Wrong")
+    exit(1)
+}
+print("What is 9+9")
+if let math = readLine(), math == "18" {
+    print("Thats right")
+} else {
+    print("Wrong")
+    exit(1)
+}
+print("What is 10+10")
+if let math = readLine(), math == "20" {
+    print("Thats right")
+} else {
+    print("Wrong")
+}
+// The last part of the original script seems incomplete and does not provide a question or a correct answer to check against.
+// Therefore, it is not translated into Swift.
+
