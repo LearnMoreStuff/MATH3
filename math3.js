86efbe3216daf65ac0974d967e1608ca9bb60da7
CloIT <105884295+Clo612@users.noreply.github.com>
2024-01-29 16:32:34 -0600

    Fixed math3.js
    We fixed it

--- math3.js
+++ math3.js
@@ -1,49 +1,44 @@
-console.log("Hi " + name + " thank you for getting MATH3 we hope you enjoy the new and improved MATH!!!");
-setTimeout(() => {
-  console.log("If you get the math wrong you start over!!");
-}, 2000);
+const readline = require('readline');
 
-setTimeout(() => {
-  console.log("What is " + random);
-  const math = prompt("Enter your answer:");
-  if (math === "2") {
-    console.log("That's right");
-  } else {
-    console.log("Wrong");
-    process.exit(1);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Hello, What is your name?");
+
+rl.question("", function(name) {
+  console.log(`Hi ${name} thank you for getting MATH3. We hope you enjoy the new and improved MATH!!!`);
+
+  setTimeout(function() {
+    console.log("If you get the math wrong you start over!!");
+    setTimeout(askQuestion, 1000);
+  }, 1000);
+
+  function askQuestion() {
+    rl.question("What is 1+1? ", function(math) {
+      if (parseInt(math) === 2) {
+        console.log("Thats right");
+        setTimeout(askQuestion2, 2000);
+      } else {
+        console.log("Wrong");
+        process.exit(1);
+      }
+    });
   }
-}, 4000);
 
-setTimeout(() => {
-  console.log("What is 2+2");
-  const math = prompt("Enter your answer:");
-  if (math === "4") {
-    console.log("That's right");
-  } else {
-    console.log("Wrong");
-    process.exit(1);
+  function askQuestion2() {
+    rl.question("What is 2+2? ", function(math) {
+      if (parseInt(math) === 4) {
+        console.log("Thats right");
+        // Continue with other questions...
+        rl.close();
+      } else {
+        console.log("Wrong");
+        process.exit(1);
+      }
+    });
   }
-}, 6000);
 
-// ... Repeat the pattern for the other math questions ...
-
-setTimeout(() => {
-  console.log("What is 10+10");
-  const math = prompt("Enter your answer:");
-  if (math === "20") {
-    console.log("That's right");
-  } else {
-    console.log("Wrong");
-  }
-}, 18000);
-
-setTimeout(() => {
-  console.log("What is ");
-  const math = prompt("Enter your answer:");
-  if (math === "20") {
-    console.log("That's right");
-  } else {
-    console.log("Wrong");
-  }
-}, 20000);
+});
 
