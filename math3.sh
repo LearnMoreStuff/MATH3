18453817a463a411e189c5476d790ee95f9a9bab
CloIT <105884295+Clo612@users.noreply.github.com>
2024-03-13 17:51:11 -0500

    Added the MATH3 new bash

--- math3.sh
+++ math3.sh
@@ -1,136 +1,52 @@
 #!/bin/bash
 
-echo "Hello, What is your name?"
+# Let's define our questions and answers
+questions=("What's 1 + 1?" "What's 2 + 2?" "What's 3 + 3?" "What's 4 + 4?" "What's 5 + 5?"
+           "What's 6 + 6?" "What's 7 + 7?" "What's 8 + 8?" "What's 9 + 9?" "What's 10 + 10?" "What's the capital of France?")
+answers=(2 4 6 8 10 12 14 16 18 20 "Paris")
 
-read name
+# Function to check answer and give response
+check_answer() {
+    local user_answer="$1"
+    local correct_answer="${answers[$question_number]}"
+    
+    if [[ $user_answer == $correct_answer ]]; then
+        if [[ $correct_answer == "Paris" ]]; then
+            echo "Très bien! That's correct."
+        else
+            echo "Jolly good! That's right."
+        fi
+        ((score++))
+    elif [[ $user_answer =~ ^[0-9]+$ ]]; then
+        echo "Oh dear, that's not quite right."
+    else
+        echo "Oh dear, that doesn't seem to be a valid answer."
+    fi
+}
 
-echo "Hi $name thank you for getting MATH3 we hope you enjoy the new and improved MATH!!!"
+# Initialize score
+score=0
 
-sleep 1
+# Shuffle questions and answers using Fisher-Yates algorithm
+for (( i=${#questions[@]}-1; i>0; i-- )); do
+    j=$((RANDOM % (i+1)))
+    temp=${questions[i]}
+    questions[i]=${questions[j]}
+    questions[j]=$temp
+    
+    temp=${answers[i]}
+    answers[i]=${answers[j]}
+    answers[j]=$temp
+done
 
-echo "If you get the math wrong you start over!!"
+# Loop through each question
+for ((question_number = 0; question_number < ${#questions[@]}; question_number++)); do
+    # Ask the question and get the user's answer
+    read -p "${questions[$question_number]} " user_answer
+    
+    # Check if the answer is correct
+    check_answer "$user_answer"
+done
 
-sleep 1
-
-sleep 1
-
-echo "What is 1+1"
-
-read math
-
-if [[ 2 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-exit 1
-fi
-
-sleep 2
-
-echo "What is 2+2"
-
-read math
-
-if [[ 4 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-exit 1
-fi
-
-echo "What is 3+3"
-
-read math
-
-if [[ 6 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-exit 1
-fi
-
-echo "What is 4+4"
-
-read math
-
-if [[ 8 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-exit 1
-fi
-
-echo "What is 5+5"
-
-read math
-
-if [[ 10 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-exit 1
-fi
-
-echo "What is 6+6"
-
-read math
-
-if [[ 12 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-exit 1
-fi
-
-echo "What is 7+7"
-
-read math
-
-if [[ 14 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-exit 1
-fi
-
-echo "What is 8+8"
-
-read math
-
-if [[ 16 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-exit 1
-fi
-
-echo "What is 9+9"
-
-read math
-
-if [[ 18 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-exit 1
-fi
-
-echo "What is 10+10"
-
-read math
-
-if [[ 20 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-fi
-
-echo "What is "
-
-read math
-
-if [[ 20 == $math ]]; then
-echo "Thats right"
-else
-echo "Wrong"
-fi
+# Display final score
+echo "Your score is $score out of ${#questions[@]}."
